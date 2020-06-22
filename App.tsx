59b924ae6c4c6222a1bd9ed5bcf9d1aea358c977
Patryk <musiol.patryk97@gmail.com>
2020-06-22 21:31:44 +0200

    Artur's help required

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View } from "react-native";
 import PublicationList from "./src/ui/publikacje/PublicationList";
-export default function App() {
-  return <PublicationList />;
-}
+import DetailsForm from "./src/ui/szczegoly/DetailsForm";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={PublicationList}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="form" component={DetailsForm} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
